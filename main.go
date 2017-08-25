04dbaf540303f9a7f4235ac840ba9a2b8dfdf690
themaxaboy <themaxaboy@hotmail.com>
2017-08-25 17:55:18 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,42 @@
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !plan9
+
+package main
+
+import (
+	"log"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				/*log.Println("event:", event)*/
+				if event.Op&fsnotify.Create == fsnotify.Create {
+					log.Println(event.Name)
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add("D:\\Main SVN\\news\\working")
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
