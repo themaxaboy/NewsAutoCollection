dbd128cdda1eef3ee6092c9d4e2bbb449eb9bf5b
Themaxaboy <themaxaboy@hotmail.com>
2017-08-30 02:44:44 +0700

    add .bat

---
+++ installer.bat
@@ -0,0 +1,3 @@
+sc create "News Auto Collection" binPath="%~dp0NewsAutoCollection.exe"
+sc description "News Auto Collection" "Service for News Auto Collection."
+timeout /t 10
---
+++ uninstall.bat
@@ -0,0 +1,3 @@
+sc stop "News Auto Collection"
+sc delete "News Auto Collection"
+timeout /t 10
